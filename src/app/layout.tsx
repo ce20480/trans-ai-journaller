d5c4cb3ae85cf12eb184bf7f1292a43142255391
ce20480 <aaronvinod25@gmail.com>
2025-04-16 10:26:22 +0100

    Added analytics and speed insights from next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -26,6 +26,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
