3a8274b2519a29aa4c91770259827690ce41145b
ce20480 <aaronvinod25@gmail.com>
2025-04-17 13:25:39 +0100

    Fixed tailwind issues and relative

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,19 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
 
 // const config = {
 //   plugins: {
 //     tailwindcss: {},
 //   },
 // };
+
+// export default config;
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
 
 export default config;
